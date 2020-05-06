ff8ac38ee7dbae15573e1303c227378242ee41e6
mickaelchoidev <mickaelchoidev@gmail.com>
2020-05-06 13:58:18 +0200

    header ok, working on footer for icons

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      className="container-fluid p-0 m-0 bg-dark text-light fixed-bottom"
+      id="footer"
+    >
+      <div className="row p-0 m-0">
+        <h5 className="col-md px-3 m-0">
+          Budget App designed by Mickael Choi, React Developer, 2020
+        </h5>
+        <div className="col-md px-3 m-0"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="container-fluid p-0 m-0">
+      <h1
+        className="text-center px-1 py-2 m-0 bg-primary text-light"
+        id="title-header"
+      >
+        BUDGET APP
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
